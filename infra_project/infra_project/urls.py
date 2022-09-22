d932ff450c13e18d23f9e5fc8eb30d875296ec66
kissboing <dan-90@yandex.ru>
2022-09-23 00:09:25 +0300

    fix flake8 test

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path('', include('infra_app.urls', namespace='infra_app')),
     path('admin/', admin.site.urls),
+    path('', include('infra_app.urls', namespace='infra_app')),
 ]
